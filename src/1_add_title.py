95f55ae549cd8c7e24a546c15ba2725371b8785f
Thorben Jensen <jensen.thorben@googlemail.com>
2018-04-21 18:51:55 +0200

    reformatting

--- src/1_add_title.py
+++ src/1_add_title.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import numpy as np
 import html
-#/Users/jpapaioannou/Documents/Repos/private/dssg2018/hackathon-borderstep/
+
+
+# /Users/jpapaioannou/Documents/Repos/private/dssg2018/hackathon-borderstep/
 BASE_DATA_PATH = 'data/processed/'
-                 #'word_count_2015_2016.pkl'
+# 'word_count_2015_2016.pkl'
 BASE_DATA_PATH_RESULTS = 'data/processed/DSSG/'
 
-def get_data(file_path):
 
+def get_data(file_path):
     if 'xlsx' in file_path:
         data = pd.read_excel(BASE_DATA_PATH + file_path)
 
     elif 'pkl' in file_path:
         data = pd.read_pickle(BASE_DATA_PATH + file_path)
 
-
     return data
+
 
 def extract_title_from_html(ex):
     from bs4 import BeautifulSoup
@@ -24,6 +26,7 @@ def extract_title_from_html(ex):
         for header in soup.find_all('h1'):
             if header:
                 yield header.get_text().translate(str.maketrans("\n\t\r\xa0", "    ")).strip()
+
 
 """
 def read_files_from_data_folder(filenames):
@@ -39,18 +42,19 @@ def read_files_from_data_folder(filenames):
         domain_info.to_pickle(filepath, compression='infer', protocol=4)
 """
 
+
 def extract_text_from_title(x):
     html_text = x.values.item()
     headers = ' '.join(extract_title_from_html(html_text))
     return headers
 
-def read_files_from_data_folder(filenames):
 
+def read_files_from_data_folder(filenames):
     for filename in filenames:
         domain_info = get_data(filename)
-        domain_info['titles'] = domain_info[['html']].dropna().apply(lambda x: extract_text_from_title(x),
-                                                                     axis=1
-                                                                     )
+        domain_info['titles'] = \
+            domain_info[['html']].dropna().apply(lambda x: extract_text_from_title(x),
+                                                 axis=1)
         end = filename.find('.pkl.gz')
         filename = filename[:end]
         filepath = BASE_DATA_PATH_RESULTS+filename+'_with_titles.pkl.gz'
@@ -58,8 +62,7 @@ def read_files_from_data_folder(filenames):
 
 
 if __name__ == '__main__':
-
     #filenames = ['GEMO-GrüneUN2006-2013.pkl.gz', 'GEMO_2015.pkl.gz', 'GEMO_2016.pkl.gz']
-
     filenames = ['word_count_2015_2016.pkl']
     read_files_from_data_folder(filenames)
+
